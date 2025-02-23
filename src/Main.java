fe26e37ede29b99a66dae70f45bca269f31b8b11
MortiestMorty <d.arroyo14@student.keiseruniversity.edu>
2025-02-23 16:02:47 -0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+how selection sort sorts the array E A S Y
+Q U E S T I O N.Show, in the style of the example trace
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+public class Selection
+{
+public static void sort(Comparable[] a)
+{ // Sort a[] into increasing order.
+int N = a.length; // array length
+for (int i = 0; i < N; i++)
+{ // Exchange a[i] with smallest entry in a[i+1...N).
+int min = i; // index of minimal entr.
+for (int j = i+1; j < N; j++)
+if (less(a[j], a[min])) min = j;
+exch(a, i, min);
 }
+}
+ */
+import java.util.Arrays;
+
+public class Main
+{
+    public static void sort (Comparable[] a)
+    {
+        int N = a.length;
+
+        for(int i = 0;i < N;i++)
+        {
+            int min = i;
+
+            for( int j = i + 1;j < N;j++)
+            {
+                if (less(a [j], a [min]))
+                {
+                    min = j;
+                }// end if statement
+            }//end nested for loop
+
+        exch(a,i, min);
+
+            System.out.println("pass:" + ( i + 1) + " :" + Arrays.toString(a));
+
+        }// end first for loop
+    }// end sort method
+
+    private static boolean less (Comparable v,Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }// end private method
+
+
+    private static void exch (Comparable[] a, int i, int j)
+    {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }// end exch method
+
+    public static void main (String[] args)
+    {
+        Character [] arr = {'E','A','S','Y','Q','U','E','S','T','I','O','N'};
+
+        System.out.println( "OG Array:" + Arrays.toString(arr));
+        sort(arr);
+        System.out.println( "Sorted Array:" + Arrays.toString(arr));
+
+    }// end main
+}// end class
